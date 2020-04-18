bc884e0a708b2137dd3630621259a3c3946391c7
Benjamin Haas <haasbenjam@gmail.com>
2020-04-18 17:28:37 +0200

    Add script to test an agent.

---
+++ test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from unityagents import UnityEnvironment
+import numpy as np
+
+from common import BANANA_FILE
+from dqn_agent import Agent
+
+# Get environment instance
+env = UnityEnvironment(file_name=BANANA_FILE)
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+# get initial state
+state = env_info.vector_observations[0]
+# get state size and actions size
+state_size = len(state)
+action_size = brain.vector_action_space_size
+# Create Agent instance
+agent = Agent(state_size=state_size, action_size=action_size, seed=0, weights='weights.pth')
+# Initialize the score
+score = 0
+done = False
+# Test the Agent
+while not done:
+    action = agent.act(state)                      # select an action
+    env_info = env.step(action)[brain_name]        # send the action to the environment
+    state = env_info.vector_observations[0]        # update the state
+    reward = env_info.rewards[0]                   # get the reward
+    done = env_info.local_done[0]                  # see if episode has finished
+    score += reward                                # update the score
+    if done:                                       # exit loop if episode finished
+        break
+print("Score: {}".format(score))
+env.close()
